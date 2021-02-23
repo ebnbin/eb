7bb72897d5f26a47d1529700bbfb1e4ca287bf1e
Ebn Zhang <ebnbin@gmail.com>
2021-02-23 23:11:41 +0800

    Migrate to kts

---
+++ lib/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdkVersion(30)
+    defaultConfig {
+        minSdkVersion(21)
+        targetSdkVersion(30)
+    }
+}
