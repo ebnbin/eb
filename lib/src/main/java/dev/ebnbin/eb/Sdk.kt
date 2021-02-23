0ad94c98a9484495721ac659b5d5910d0e6c879f
Ebn Zhang <ebnbin@gmail.com>
2021-02-24 00:31:08 +0800

    Update Sdk

--- lib/src/main/java/dev/ebnbin/eb/Sdk.kt
+++ lib/src/main/java/dev/ebnbin/eb/Sdk.kt
@@ -2,33 +2,13 @@ package dev.ebnbin.eb
 
 import android.os.Build
 
-const val sdk21L5 = Build.VERSION_CODES.LOLLIPOP
-const val sdk22L51 = Build.VERSION_CODES.LOLLIPOP_MR1
-const val sdk23M6 = Build.VERSION_CODES.M
-const val sdk24N7 = Build.VERSION_CODES.N
-const val sdk25N71 = Build.VERSION_CODES.N_MR1
-const val sdk26O8 = Build.VERSION_CODES.O
-const val sdk27O81 = Build.VERSION_CODES.O_MR1
-const val sdk28P9 = Build.VERSION_CODES.P
-const val sdk29Q10 = Build.VERSION_CODES.Q
-const val sdk30R11 = Build.VERSION_CODES.R
-
-fun sdk21L5() = Build.VERSION.SDK_INT >= sdk21L5
-
-fun sdk22L51() = Build.VERSION.SDK_INT >= sdk22L51
-
-fun sdk23M6() = Build.VERSION.SDK_INT >= sdk23M6
-
-fun sdk24N7() = Build.VERSION.SDK_INT >= sdk24N7
-
-fun sdk25N71() = Build.VERSION.SDK_INT >= sdk25N71
-
-fun sdk26O8() = Build.VERSION.SDK_INT >= sdk26O8
-
-fun sdk27O81() = Build.VERSION.SDK_INT >= sdk27O81
-
-fun sdk28P9() = Build.VERSION.SDK_INT >= sdk28P9
-
-fun sdk29Q10() = Build.VERSION.SDK_INT >= sdk29Q10
-
-fun sdk30R11() = Build.VERSION.SDK_INT >= sdk30R11
+const val SDK_21_L_5 = Build.VERSION_CODES.LOLLIPOP
+const val SDK_22_L_5_1 = Build.VERSION_CODES.LOLLIPOP_MR1
+const val SDK_23_M_6 = Build.VERSION_CODES.M
+const val SDK_24_N_7 = Build.VERSION_CODES.N
+const val SDK_25_N_7_1 = Build.VERSION_CODES.N_MR1
+const val SDK_26_O_8 = Build.VERSION_CODES.O
+const val SDK_27_O_8_1 = Build.VERSION_CODES.O_MR1
+const val SDK_28_P_9 = Build.VERSION_CODES.P
+const val SDK_29_Q_10 = Build.VERSION_CODES.Q
+const val SDK_30_R_11 = Build.VERSION_CODES.R
