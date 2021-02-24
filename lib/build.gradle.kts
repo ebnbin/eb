06f26100978de4670d4bf695a3dfc4e0cc83d0e9
Ebn Zhang <ebnbin@gmail.com>
2021-02-24 14:27:21 +0800

    Update build.gradle.kts

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -10,6 +10,13 @@ android {
         minSdkVersion(21)
         targetSdkVersion(30)
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
