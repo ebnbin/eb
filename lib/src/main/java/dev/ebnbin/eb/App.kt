6aeac925aab82ebb12c083a61e8a43ecff8e0822
Ebn Zhang <ebnbin@gmail.com>
2021-03-03 16:07:03 +0800

    App

--- lib/src/main/java/dev/ebnbin/eb/App.kt
+++ lib/src/main/java/dev/ebnbin/eb/App.kt
@@ -1,6 +1,8 @@
 package dev.ebnbin.eb
 
 import android.app.Application
+import android.graphics.drawable.Drawable
+import android.os.Build
 
 val app: Application
     get() = EBInitializer.app
@@ -11,3 +13,23 @@ inline fun <reified T : Application> app(): T {
 
 val appId: String
     get() = app.packageName
+
+val appVersionCode: Int
+    get() {
+        val packageInfo = app.packageManager.getPackageInfo(appId, 0)
+        return if (Build.VERSION.SDK_INT >= SDK_28_P_9) {
+            packageInfo.longVersionCode.toInt()
+        } else {
+            @Suppress("DEPRECATION")
+            packageInfo.versionCode
+        }
+    }
+
+val appVersionName: String
+    get() = app.packageManager.getPackageInfo(appId, 0).versionName
+
+val appLabel: CharSequence
+    get() = app.packageManager.getApplicationLabel(app.applicationInfo)
+
+val appIcon: Drawable
+    get() = app.packageManager.getApplicationIcon(app.applicationInfo)
